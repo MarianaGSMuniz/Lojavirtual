8b5a8462de1ede4a4b73aa59b776faef6e409dd4
Mariana Muniz <101331946+MarianaGSMuniz@users.noreply.github.com>
2022-05-22 19:56:52 -0300

    continuação da  API

--- app.js
+++ app.js
@@ -7,4 +7,13 @@ const jwt = require('jsonwebtoken')
 
 const app = express()
 
+//open route
+app.get('/', (req, res) => {
+    res.status(200).json({ msg: 'Bem vindo!' })
+})
+
+mongoose.connect().then(() => {
+    app.listen(3000)
+    console.log('Conectou ao Banco!')
+}).catch((err) => console.log(err))
 app.listen(3000)
