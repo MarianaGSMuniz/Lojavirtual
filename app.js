8596cd2fbd807594dea2f46a9a19b88b3f4ff113
Mariana Muniz <101331946+MarianaGSMuniz@users.noreply.github.com>
2022-05-23 16:53:22 -0300

    continuação de codigo

--- app.js
+++ app.js
@@ -7,12 +7,33 @@ const jwt = require('jsonwebtoken')
 
 const app = express()
 
+//JSON
+app.user(express.json())
+
+//models
+const User = require('./models/User')
+
 //open route
 app.get('/', (req, res) => {
     res.status(200).json({ msg: 'Bem vindo!' })
 })
 
-mongoose.connect().then(() => {
+//Register User 
+app.post('/auth/register', async(req, res) => {
+
+    const { name, email, password, confirmpassword } = req.body
+
+    //validação
+    if (!name) {
+        return res.status(422).json({ msg: 'O nome é obrigatório!' })
+    }
+})
+
+//credenciais
+const dbUser = process.env.DB_USER
+const dbPassword = procesDB_PASS
+
+mongoose.connect('mongodb+srv://${dbUser}:${dbPassword }@cluster0.s1y1q.mongodb.net/?retryWrites=true&w=majority').then(() => {
     app.listen(3000)
     console.log('Conectou ao Banco!')
 }).catch((err) => console.log(err))
