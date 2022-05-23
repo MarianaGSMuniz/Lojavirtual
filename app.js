3e94edc78d6b92c52ae2fea67556f716283f5374
Mariana Muniz <101331946+MarianaGSMuniz@users.noreply.github.com>
2022-05-23 18:57:17 -0300

    algumas modificações

--- app.js
+++ app.js
@@ -27,6 +27,63 @@ app.post('/auth/register', async(req, res) => {
     if (!name) {
         return res.status(422).json({ msg: 'O nome é obrigatório!' })
     }
+
+    if (!email) {
+        return res.status(422).json({ msg: 'O email é obrigatório!' })
+    }
+
+    if (!passwordl) {
+        return res.status(422).json({ msg: 'A senha é obrigatório!' })
+    }
+    if (password !== confirmpassword) {
+        return res.status(422).json({ 'As senhas não conferem!' })
+    }
+
+    //check if user exists
+    const userExists = await Uswer.findOne({ email: email })
+    if (userExists) {
+
+        return res.status(422).json({ msg: 'Por favor, utilize outro e-mail!' })
+    }
+    // password 
+    const salt = await bcrypt.genSalt(12)
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    // user
+    const user = new User({
+        name,
+        email,
+        password: passwordHash,
+    })
+
+    try {
+
+        await user.save()
+        res.status(201).json({ msg: 'Uasuário criado com sucesso!' })
+
+    } catch (error) {
+
+        res.status(500).json({ msg: error })
+
+
+    }
+
+})
+
+// loggin
+
+app.post("/auth/user", async(req, res) => {
+
+    const { email, password } = req.body
+
+    if (!email) {
+        return res.status(422).json({ msg: 'O email é obrigatório!' })
+    }
+
+    if (!passwordl) {
+        return res.status(422).json({ msg: 'A senha é obrigatório!' })
+    }
+
 })
 
 //credenciais
